7655f814bffe4a0c2a27c8e92a21493793f0f005
APC <APC@LAPTOP-VHPDFFPP>
2024-05-31 17:35:52 +0700

    Add project files.

---
+++ DO_AN/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DO_AN.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DO_AN.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DO_AN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DO_AN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
